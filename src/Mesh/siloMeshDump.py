45a81ecac31719693df659a5bcd0eca952984a04
Brody Bassett <8602067+brbass@users.noreply.github.com>
2025-09-26 14:35:52 -0700

    Betrayed by right double quote

--- src/Mesh/siloMeshDump.py
+++ src/Mesh/siloMeshDump.py
@@ -538,7 +538,7 @@ def extractFields(nodeLists, time, cycle, fields,
             assert len(subfields) <= len(nodeLists)
 
             # Build the meta-data for this field entry.
-            name = str(fieldName).replace(" ", "_").replace("-", "_”)
+            name = str(fieldName).replace(" ", "_").replace("-", "_")
             varDef, varType, optlistDef, optlistMV, optlistVar = metaDataMethod(name, time, cycle, dim)
 
             # Build the complete values for this field.
