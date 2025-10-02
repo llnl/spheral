9211710b6c938d02cedf72fd12b34718261684bf
Alan Dayton <dayton8@llnl.gov>
2025-10-02 13:43:34 -0700

    Instantiations in Field

---
+++ src/Field/CoarseNodeIteratorInst.cc
@@ -0,0 +1,23 @@
+//------------------------------------------------------------------------------
+// Explicit instantiations.
+//------------------------------------------------------------------------------
+
+#include "config.hh"
+#include "Geometry/Dimension.hh"
+#include "Field/CoarseNodeIterator.cc"
+
+namespace Spheral {
+
+#if defined(SPHERAL_ENABLE_1D)
+template class CoarseNodeIterator<Dim<1>>;
+#endif
+
+#if defined(SPHERAL_ENABLE_2D)
+template class CoarseNodeIterator<Dim<2>>;
+#endif
+
+#if defined(SPHERAL_ENABLE_3D)
+template class CoarseNodeIterator<Dim<3>>;
+#endif
+
+}
---
+++ src/Field/MasterNodeIteratorInst.cc
@@ -0,0 +1,23 @@
+//------------------------------------------------------------------------------
+// Explicit instantiations.
+//------------------------------------------------------------------------------
+
+#include "config.hh"
+#include "Geometry/Dimension.hh"
+#include "Field/MasterNodeIterator.cc"
+
+namespace Spheral {
+
+#if defined(SPHERAL_ENABLE_1D)
+template class MasterNodeIterator<Dim<1>>;
+#endif
+
+#if defined(SPHERAL_ENABLE_2D)
+template class MasterNodeIterator<Dim<2>>;
+#endif
+
+#if defined(SPHERAL_ENABLE_3D)
+template class MasterNodeIterator<Dim<3>>;
+#endif
+
+}
