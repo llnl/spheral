3b57f1a36e522433d70ff09ef91a35a5cefbf2d6
Alan Dayton <dayton8@llnl.gov>
2025-10-02 14:07:27 -0700

    Instantiations in Gravity

---
+++ src/Gravity/NBodyGravityInst.cc
@@ -0,0 +1,23 @@
+//------------------------------------------------------------------------------
+// Explicit instantiations.
+//------------------------------------------------------------------------------
+
+#include "config.hh"
+#include "Geometry/Dimension.hh"
+#include "Gravity/NBodyGravity.cc"
+
+namespace Spheral {
+
+#if defined(SPHERAL_ENABLE_1D)
+template class NBodyGravity<Dim<1>>;
+#endif
+
+#if defined(SPHERAL_ENABLE_2D)
+template class NBodyGravity<Dim<2>>;
+#endif
+
+#if defined(SPHERAL_ENABLE_3D)
+template class NBodyGravity<Dim<3>>;
+#endif
+
+}
