7c37ae6e5d55e0fea00ddd4118ebae54b3520526
Alan Dayton <dayton8@llnl.gov>
2025-10-02 14:01:43 -0700

    Instantiations in FVCRKH

---
+++ src/FVCRKH/FVCRKHydroBaseInst.cc
@@ -0,0 +1,23 @@
+//------------------------------------------------------------------------------
+// Explicit instantiations.
+//------------------------------------------------------------------------------
+
+#include "config.hh"
+#include "Geometry/Dimension.hh"
+#include "FVCRKHydroBase.cc"
+
+namespace Spheral {
+
+#if defined(SPHERAL_ENABLE_1D)
+template class FVCRKHydroBase<Dim<1>>;
+#endif
+
+#if defined(SPHERAL_ENABLE_2D)
+template class FVCRKHydroBase<Dim<2>>;
+#endif
+
+#if defined(SPHERAL_ENABLE_3D)
+template class FVCRKHydroBase<Dim<3>>;
+#endif
+
+}
