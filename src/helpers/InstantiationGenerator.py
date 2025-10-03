2d0ab2d67fe606304684dc245de87756149a2b59
Alan Dayton <dayton8@llnl.gov>
2025-10-03 08:29:10 -0700

    Modify InstantiationGenerator.py to produce an instantiation file with a single or with multiple dimensions

--- src/helpers/InstantiationGenerator.py
+++ src/helpers/InstantiationGenerator.py
@@ -7,23 +7,42 @@
 #   outfile - the file to be written out
 #      ndim - an integer value for the dimensionality being generated (1,2,3)
 #-------------------------------------------------------------------------------
+import re
 import sys
 
-assert len(sys.argv) == 4
+assert len(sys.argv) >= 4
 infile = sys.argv[1]
 outfile = sys.argv[2]
-ndim = sys.argv[3]
-idim = int(ndim)
+dims = [int(x) for x in sys.argv[3:]]
 
-dictionary = {"ndim"      : ndim,
-              "Dim"       : "Dim<%s>" % ndim,
-              "Scalar"    : "Dim<%s>::Scalar" % ndim,
-              "Vector"    : "Dim<%s>::Vector" % ndim,
-              "Tensor"    : "Dim<%s>::Tensor" % ndim,
-              "SymTensor" : "Dim<%s>::SymTensor" % ndim,
-}
-
-# Read the input file to get the definition of the string "text", which we use to generate the explicit instantiation .cc file
+# Read the input file to get the definition of the string "text",
+# which we use to generate the explicit instantiation .cc file
 exec(open(infile).read())
+
+# Parse "text" into a header, instantiations, and footer. We need to make sure headers are only included once since many of them do not have include guards.
+index = re.search("^[ \t]*template", text, re.MULTILINE).start()
+header = text[:index].lstrip()
+remainder = text[index:]
+index = remainder.rfind("}")
+instantiations = remainder[:index].rstrip()
+footer = remainder[index:].lstrip()
+
+# Build up the text to write out
+outtext = header
+
+for ndim in dims:
+    dictionary = {"ndim"      : ndim,
+                  "Dim"       : "Dim<%s>" % ndim,
+                  "Scalar"    : "Dim<%s>::Scalar" % ndim,
+                  "Vector"    : "Dim<%s>::Vector" % ndim,
+                  "Tensor"    : "Dim<%s>::Tensor" % ndim,
+                  "SymTensor" : "Dim<%s>::SymTensor" % ndim,
+    }
+
+    outtext += f"{instantiations % dictionary}\n"
+
+outtext += footer
+outtext = outtext.rstrip()
+
 with open(outfile, "w") as f:
-    f.write(text % dictionary)
+    f.write(outtext)
