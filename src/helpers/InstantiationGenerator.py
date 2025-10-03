92885145c414c65f85df174c0c6e1918d4d3ef75
Alan Dayton <dayton8@llnl.gov>
2025-10-03 09:02:09 -0700

    Clean up InstantiationGenerator.py

--- src/helpers/InstantiationGenerator.py
+++ src/helpers/InstantiationGenerator.py
@@ -3,46 +3,76 @@
 # 
 # A python script to automatically generate Spheral++ instantion files to be 
 # compiled.  Assumed arguments:
-#    infile - the file to be read, defining "text"
-#   outfile - the file to be written out
+#    input_file - the file to be read, defining "text"
+#   output_file - the file to be written out
 #      ndim - an integer value for the dimensionality being generated (1,2,3)
 #-------------------------------------------------------------------------------
+import argparse
 import re
-import sys
 
-assert len(sys.argv) >= 4
-infile = sys.argv[1]
-outfile = sys.argv[2]
-dims = [int(x) for x in sys.argv[3:]]
+def generate_instantiations(input_file, output_file, dimensions):
+    # Read the input file to get the definition of the string "text",
+    # which we use to generate the explicit instantiation .cc file
+    variables = {}
 
-# Read the input file to get the definition of the string "text",
-# which we use to generate the explicit instantiation .cc file
-exec(open(infile).read())
+    with open(input_file) as f:
+        exec(f.read(), variables)
 
-# Parse "text" into a header, instantiations, and footer. We need to make sure headers are only included once since many of them do not have include guards.
-index = re.search("^[ \t]*template", text, re.MULTILINE).start()
-header = text[:index].lstrip()
-remainder = text[index:]
-index = remainder.rfind("}")
-instantiations = remainder[:index].rstrip()
-footer = remainder[index:].lstrip()
+    text = variables["text"]
 
-# Build up the text to write out
-outtext = header
+    # Parse "text" into a header, instantiations, and footer.
+    index = re.search("^[ \t]*template", text, re.MULTILINE).start()
+    header = text[:index]
+    remainder = text[index:]
 
-for ndim in dims:
-    dictionary = {"ndim"      : ndim,
-                  "Dim"       : "Dim<%s>" % ndim,
-                  "Scalar"    : "Dim<%s>::Scalar" % ndim,
-                  "Vector"    : "Dim<%s>::Vector" % ndim,
-                  "Tensor"    : "Dim<%s>::Tensor" % ndim,
-                  "SymTensor" : "Dim<%s>::SymTensor" % ndim,
-    }
+    index = remainder.rfind("}")
+    instantiations = remainder[:index].rstrip()
+    footer = remainder[index:].lstrip()
 
-    outtext += f"{instantiations % dictionary}\n"
+    # Build up the text to write out
+    output_text = header
 
-outtext += footer
-outtext = outtext.rstrip()
+    for ndim in dimensions:
+        dictionary = {"ndim"      : ndim,
+                      "Dim"       : "Dim<%s>" % ndim,
+                      "Scalar"    : "Dim<%s>::Scalar" % ndim,
+                      "Vector"    : "Dim<%s>::Vector" % ndim,
+                      "Tensor"    : "Dim<%s>::Tensor" % ndim,
+                      "SymTensor" : "Dim<%s>::SymTensor" % ndim,
+        }
 
-with open(outfile, "w") as f:
-    f.write(outtext)
+        output_text += f"{instantiations % dictionary}\n"
+
+    output_text += footer
+    output_text = output_text.strip()
+
+    with open(output_file, "w") as f:
+        f.write(output_text)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Generate a C++ instantiation file from a template string."
+    )
+
+    parser.add_argument(
+        "input_file",
+        type=str,
+        help="Input file containing the template as a Python string variable 'text'."
+    )
+
+    parser.add_argument(
+        "output_file",
+        type=str,
+        help="Output file to write the generated instantiations."
+    )
+
+    parser.add_argument(
+        "dimensions",
+        type=int,
+        nargs="+",
+        help="One or more dimensions for which to generate instantiations (e.g., 1 2 3)."
+    )
+
+    args = parser.parse_args()
+    generate_instantiations(args.input_file, args.output_file, args.dimensions)
