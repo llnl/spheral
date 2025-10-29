dfce6204b893aff073d1b8c0b8ae07cdb6a81ae5
Landon Owen <lndnwn9@gmail.com>
2025-10-29 15:34:04 -0700

    Moved QIView and CHIView classes to separate files

--- src/Utilities/QuadraticInterpolator.hh
+++ src/Utilities/QuadraticInterpolator.hh
@@ -9,6 +9,7 @@
 #ifndef __Spheral_QuadraticInterpolator__
 #define __Spheral_QuadraticInterpolator__
 
+#include "QIView.hh"
 #include "chai/ManagedArray.hpp"
 #include "config.hh"
 
@@ -16,49 +17,6 @@
 #include <vector>
 
 namespace Spheral {
-
-class QIView {
-public:
-  using ContainerType = typename chai::ManagedArray<double>;
-  //--------------------------- Public Interface ---------------------------//
-  // Constructors, destructors
-  SPHERAL_HOST_DEVICE QIView() = default;
-
-  // Comparisons
-  SPHERAL_HOST_DEVICE bool operator==(const QIView& rhs) const;
-
-  // Interpolate for the y value
-  SPHERAL_HOST_DEVICE double operator()(const double x) const;
-  SPHERAL_HOST_DEVICE double prime(const double x) const;    // First derivative
-  SPHERAL_HOST_DEVICE double prime2(const double x) const;   // Second derivative
-  // Index access
-  SPHERAL_HOST_DEVICE double operator[](const size_t i) const;
-
-  // Same as above, but use a pre-computed table position (from lowerBound)
-  SPHERAL_HOST_DEVICE double operator()(const double x, const size_t i0) const;
-  SPHERAL_HOST_DEVICE double prime(const double x, const size_t i0) const;    // First derivative
-  SPHERAL_HOST_DEVICE double prime2(const double x, const size_t i0) const;   // Second derivative
-
-  // Return the lower bound index in the table for the given x coordinate
-  SPHERAL_HOST_DEVICE size_t lowerBound(const double x) const;
-
-  // Allow read access the internal data representation
-  SPHERAL_HOST_DEVICE size_t size() const;                        // The size of the tabulated coefficient arrays
-  SPHERAL_HOST_DEVICE double xmin() const;                        // Minimum x coordinate for table
-  SPHERAL_HOST_DEVICE double xmax() const;                        // Maximum x coordinate for table
-  SPHERAL_HOST_DEVICE double xstep() const;                       // delta x between tabulated values
-  void move(chai::ExecutionSpace space) { mcoeffs.move(space); }
-  SPHERAL_HOST_DEVICE double* data() const { return mcoeffs.data(); }
-
-protected:
-  //--------------------------- Private Interface --------------------------//
-  // Member data
-  size_t mN1 = 0u;
-  double mXmin = 0.;
-  double mXmax = 0.;
-  double mXstep = 0.;
-  ContainerType mcoeffs;
-};
 
 class QuadraticInterpolator : public QIView {
 public:
