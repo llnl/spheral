9a38063c6d471ad43c63c334dbe203e7a320d992
Paul H. Tsuji <tsuji1@llnl.gov>
2025-11-14 09:00:44 -0800

    Revert "Add the #ifndef for CHAI_DISABLE_RM here."
    This reverts commit 647782161d4eaa5c66639b927d72c5d1bb035648.

--- src/Neighbor/NodePairList.hh
+++ src/Neighbor/NodePairList.hh
@@ -90,9 +90,7 @@ public:
 
   template<typename F> inline
   void setUserCallback(F&& extension) {
-#ifndef CHAI_DISABLE_RM
     mData.setUserCallback(getNPLCallback(std::forward<F>(extension)));
-#endif
   }
 
 protected:
