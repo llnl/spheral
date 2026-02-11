817cf093574d4825502d4e11eddc9b11dd0c8ad1
Landon Owen <lndnwn9@gmail.com>
2026-02-11 10:21:12 -0800

    More typos

--- scripts/lc/plot_hist_data.py
+++ scripts/lc/plot_hist_data.py
@@ -154,10 +154,10 @@ def main():
     cur_dir = os.getcwd()
     doc_dir = os.path.join(cur_dir, args.doc_dir)
     if (args.pkg_name == "Spheral"):
-        sys.append("../../tests/performance")
+        sys.path.append("../../tests/performance")
         import perf_tests as pt
     else:
-        sys.append("../../../tests/performance")
+        sys.path.append("../../../tests/performance")
         import llnl_perf_tests as pt
     if (rank == 0):
         if (os.path.exists(doc_dir)):
