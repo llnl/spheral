3d6bebc136075a3fa89df1b93555c36fb2f4fe4c
Mike Owen <mikeowen@llnl.gov>
2026-02-18 16:06:43 -0800

    Fiddling with Q application

--- src/SPH/SPHRZ.cc
+++ src/SPH/SPHRZ.cc
@@ -576,8 +576,8 @@ evaluateDerivativesImpl(const Dim<2>::Scalar time,
       // Acceleration.
       CHECK(rhoi > 0.0);
       CHECK(rhoj > 0.0);
-      const auto deltaDvDti = mRZj*((Pi*rhoj/(rhoi*rhoi)*gradWi + Pj/rhoj*gradWj)/rhoRZj + Qacci + Qaccj);  // More rigorous definition
-      const auto deltaDvDtj = mRZi*((Pj*rhoi/(rhoj*rhoj)*gradWj + Pi/rhoi*gradWi)/rhoRZi + Qacci + Qaccj);
+      const auto deltaDvDti = mRZj*(((Pi + Qi)*rhoj/(rhoi*rhoi)*gradWi + (Pj + Qj)/rhoj*gradWj)/rhoRZj);  // More rigorous definition
+      const auto deltaDvDtj = mRZi*(((Pj + Qj)*rhoi/(rhoj*rhoj)*gradWj + (Pi + Qi)/rhoi*gradWi)/rhoRZi);
       // const auto deltaDvDti = (Pi*gradWi + Pj*gradWj)/(rhoi*rhoRZj) + Qacci + Qaccj;
       // const auto deltaDvDtj = (Pi*gradWi + Pj*gradWj)/(rhoRZi*rhoj) + Qacci + Qaccj;
       // const auto Prhoi = safeOmegai*Pi/(rhoi*rhoRZi);
@@ -594,8 +594,8 @@ evaluateDerivativesImpl(const Dim<2>::Scalar time,
       // Specific thermal energy evolution.
       // DepsDti += 0.5*(Pi + 0.5*(Qi + Qj))*Ai*Aj*vij.dot(gradWi - gradWj);   // CRK similar form
       // DepsDtj += 0.5*(Pj + 0.5*(Qi + Qj))*Ai*Aj*vij.dot(gradWi - gradWj);
-      DepsDti += mRZj*(Pi/rhoi*vij.dot(gradWi)/rhoRZj + workQi);    // More rigorous definition
-      DepsDtj += mRZi*(Pj/rhoj*vij.dot(gradWj)/rhoRZi + workQj);
+      DepsDti += mRZj*((Pi + 0.5*Qi)/rhoi*vij.dot(gradWi)/rhoRZj);    // More rigorous definition
+      DepsDtj += mRZi*((Pj + 0.5*Qj)/rhoj*vij.dot(gradWj)/rhoRZi);
       // DepsDti += mRZj*(Prhoi*vij.dot(gradWi) + workQi);
       // DepsDtj += mRZi*(Prhoj*vij.dot(gradWj) + workQj);
       // DepsDti += mRZj*(2.0*M_PI*Prhoi*vij.dot(gradWi) + workQi);
