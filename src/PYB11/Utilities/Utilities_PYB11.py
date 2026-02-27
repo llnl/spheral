c0d4b8039aed7f3922ebcf2c4376cc54e14f6734
Landon Owen <lndnwn9@gmail.com>
2026-02-27 10:13:33 -0800

    Move adiak init routine out of pyb11gen preamble to separate file, rename python env used to deploy gitlab pages in CI

--- src/PYB11/Utilities/Utilities_PYB11.py
+++ src/PYB11/Utilities/Utilities_PYB11.py
@@ -60,47 +60,16 @@ PYB11includes += ['"Utilities/setGlobalFlags.hh"',
                   '"Utilities/uniform_random.hh"',
                   '"Utilities/Timer.hh"',
                   '"Utilities/initializeAxom.hh"',
-                  '"Distributed/Communicator.hh"',
+                  '"Utilities/initializeAdiak.hh"',
                   '"adiak.hpp"',
                   '<algorithm>']
 
 #-------------------------------------------------------------------------------
 # Preamble
 #-------------------------------------------------------------------------------
-PYB11preamble += """
-namespace Spheral {
-
-inline void spheral_adiak_init() {
-  adiak::init((void*) Communicator::comm_ptr());
-  // Always collect some curated default adiak information
-  adiak::adiakversion();
-  adiak::user();
-  adiak::uid();
-  adiak::launchdate();
-  adiak::workdir();
-  adiak::hostname();
-  adiak::clustername();
-  adiak::walltime();
-  adiak::cputime();
-  adiak::jobsize();
-  adiak::numhosts();
-  adiak::hostlist();
-  adiak::mpi_library_version();
-}
-
-enum adiak_categories {
-unset = 0,
-all,
-general,
-performance,
-control
-};
-}
-"""
-
 PYB11modulepreamble = """
 TIME_PHASE_BEGIN("main");
-Spheral::spheral_adiak_init();
+Spheral::initializeAdiak();
 
 // Call these routines when module is exited
 auto atexit = py::module_::import("atexit");
---
+++ src/Utilities/initializeAdiak.hh
@@ -0,0 +1,49 @@
+//---------------------------------Spheral++----------------------------------//
+// Initialize Spheral's use of Adiak
+//----------------------------------------------------------------------------//
+#ifndef Spheral_initializeAdiak
+#define Spheral_initializeAdiak
+
+#include "config.hh"
+#include "Distributed/Communicator.hh"
+#include "adiak.hpp"
+
+namespace Spheral {
+
+void initializeAdiak() {
+  adiak::init((void*) Communicator::comm_ptr());
+  // Always collect some curated default adiak information
+  adiak::adiakversion();
+  adiak::user();
+  adiak::uid();
+  adiak::launchdate();
+  adiak::workdir();
+  adiak::hostname();
+  adiak::clustername();
+  adiak::walltime();
+  adiak::cputime();
+  adiak::jobsize();
+  adiak::numhosts();
+  adiak::hostlist();
+  adiak::mpi_library_version();
+  // TODO: Add this when GPU testing starts
+#ifdef SPHERAL_ENABLE_HIP
+  int device_count = 0;
+  hipError_t err = hipGetDeviceCount(&device_count);
+  ASSERT(err == hipSuccess);
+  adiak::value("gpus_per_rank", device_count);
+#endif
+}
+
+enum adiak_categories
+  {
+   unset = 0,
+   all,
+   general,
+   performance,
+   control
+  };
+
+}
+
+#endif
