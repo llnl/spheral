0d189fc8457c511e2b1f4e3273039cd46a0aaf5b
Mike Owen <mikeowen@llnl.gov>
2026-03-30 15:44:25 -0700

    Checkpoint

--- src/SPH/SPHRZ.cc
+++ src/SPH/SPHRZ.cc
@@ -578,7 +578,7 @@ evaluateDerivativesImpl(const Dim<2>::Scalar time,
       // const auto  riInv = safeInvVar(ri, tiny);
       const auto  zetai = (Hi*posi).y();
       const auto  hri = ri*safeInv(zetai);
-      const auto  riInv = safeInv(ri, 0.05*hri);
+      const auto  riInv = safeInv(ri, 0.01*hri);
       // const auto  safeOmegai = safeInv(omegai, tiny);
       // const auto  Ai = mRZi/rhoRZi;
       // const auto  zetai = abs((Hi*posi).y());
@@ -615,7 +615,7 @@ evaluateDerivativesImpl(const Dim<2>::Scalar time,
       // const auto  rjInv = safeInvVar(rj, tiny);
       const auto  zetaj = (Hj*posj).y();
       const auto  hrj = rj*safeInv(zetaj);
-      const auto  rjInv = safeInv(rj, 0.05*hrj);
+      const auto  rjInv = safeInv(rj, 0.01*hrj);
       // const auto  safeOmegaj = safeInv(omegaj, tiny);
       // const auto  Aj = mRZj/rhoRZj;
       // const auto  zetaj = abs((Hj*posj).y());
