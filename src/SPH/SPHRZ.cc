8ff9db90b10d38ae3faa092efd66b4c74a871f6e
Mike Owen <mikeowen@llnl.gov>
2026-04-17 13:53:39 -0700

    A bit more safety in the mass density sum

--- src/SPH/SPHRZ.cc
+++ src/SPH/SPHRZ.cc
@@ -291,9 +291,13 @@ preStepInitialize(const DataBase<Dimension>& dataBase,
     for (auto i = 0u; i < n; ++i) {
       CHECK(massDensityRZ(k,i) > 0.0);
       const auto& posi = position(k, i);
+      const auto& Hi = H(k, i);
       const auto  ri = std::abs(posi.y());
+      const auto  zetai = std::abs((Hi*posi).y());
+      const auto  hri = ri*safeInv(zetai);
+      CHECK(hri >= 0.0);
       const auto Ai = massRZ(k,i)/massDensityRZ(k,i);
-      const auto Vi = 2.0*M_PI*ri*Ai;
+      const auto Vi = 2.0*M_PI*std::max(ri, 0.01*hri)*Ai;
       massDensity(k,i) = mass(k,i)/Vi;
       // const auto di = std::sqrt(massRZ(k,i)/massDensityRZ(k,i));
       // const auto Vi = cylindricalToroidalVolume(di, ri);
@@ -671,7 +675,7 @@ evaluateDerivativesImpl(const Dim<2>::Scalar time,
       const auto  zetai = (Hi*posi).y();            // Can be negative for ghost points!
       const auto  hri = ri*safeInv(zetai);          // Always positive
       CHECK(hri >= 0.0);
-      const auto  riInv = safeInvVar(ri, 0.001*hri);
+      const auto  riInv = safeInvVar(ri, 0.1*hri);
       const auto  numNeighborsi = connectivityMap.numNeighborsForNode(nodeListi, i);
       CHECK2(rhoi > 0.0, "Bad rho (" << nodeListi << " " << i << ") : " << rhoi);
       CHECK2(rhoRZi > 0.0, "Bad rhoRZ (" << nodeListi << " " << i << ") : " << rhoRZi);
