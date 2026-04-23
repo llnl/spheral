ac00fa0d5fbabf4c744e1d6d8648dc8c252de705
brody <brody.bassett@pacificfusion.com>
2026-04-23 21:25:05 +0000

    State lookup optimizations

--- src/DataBase/StateBase.cc
+++ src/DataBase/StateBase.cc
@@ -233,10 +233,17 @@ template<typename Dimension>
 bool
 StateBase<Dimension>::
 fieldNameRegistered(const FieldName& name) const {
-  KeyType fieldName, nodeListName;
-  for (auto [key, valptr]: mStorage) {
-    splitFieldKey(key, fieldName, nodeListName);
-    if (fieldName == name) return true;
+  // Field keys have the form <name>|<nodeListName>, so we can jump straight to the neighborhood
+  const auto prefix = name + "|";
+  auto itr = mStorage.lower_bound(prefix);
+  if (itr == mStorage.end()) return false;
+  if (itr->first.compare(0, prefix.size(), prefix) != 0) return false;
+  // Confirm this key actually refers to a Field (as opposed to a non-field entry)
+  for (; itr != mStorage.end(); ++itr) {
+    if (itr->first.compare(0, prefix.size(), prefix) != 0) break;
+    if (itr->second.type() == typeid(std::reference_wrapper<FieldBase<Dimension>>)) {
+      return true;
+    }
   }
   return false;
 }
@@ -261,7 +268,7 @@ std::vector<typename StateBase<Dimension>::KeyType>
 StateBase<Dimension>::
 fullFieldKeys() const {
   vector<KeyType> result;
-  for (auto [key, aref]: mStorage) {
+  for (const auto& [key, aref]: mStorage) {
     if (aref.type() == typeid(std::reference_wrapper<FieldBase<Dimension>>)) {
       result.push_back(key);
     }
@@ -277,7 +284,7 @@ std::vector<typename StateBase<Dimension>::KeyType>
 StateBase<Dimension>::
 miscKeys() const {
   vector<KeyType> result;
-  for (auto [key, aref]: mStorage) {
+  for (const auto& [key, aref]: mStorage) {
     if (aref.type() != typeid(std::reference_wrapper<FieldBase<Dimension>>)) {
       result.push_back(key);
     }
@@ -294,9 +301,9 @@ StateBase<Dimension>::
 fieldNames() const {
   vector<FieldName> result;
   KeyType fieldName, nodeListName;
-  for (auto [key, aref]: mStorage) {
+  for (const auto& [key, aref]: mStorage) {
     if (aref.type() == typeid(std::reference_wrapper<FieldBase<Dimension>>)) {
-      auto fref = std::any_cast<std::reference_wrapper<FieldBase<Dimension>>>(aref);
+      auto& fref = std::any_cast<const std::reference_wrapper<FieldBase<Dimension>>&>(aref);
       splitFieldKey(fref.get().name(), fieldName, nodeListName);
       result.push_back(fieldName);
     }
