12b3613df31799d2f67b8aeb722604c6e66b76f8
brody <brody.bassett@pacificfusion.com>
2026-05-27 00:10:54 +0000

    Added missing typedefs

--- src/PYB11/SPH/SPH_RAJA.py
+++ src/PYB11/SPH/SPH_RAJA.py
@@ -16,6 +16,9 @@ class SPH_RAJA(SPH):
   using SymTensor = typename %(Dimension)s::SymTensor;
   using TimeStepType = typename Physics<%(Dimension)s>::TimeStepType;
   using ResidualType = typename Physics<%(Dimension)s>::ResidualType;
+  using VolumeRequirements = typename Physics<%(Dimension)s>::VolumeRequirements;
+  using RKRequirements = typename Physics<%(Dimension)s>::RKRequirements;
+  using ConnectivityRequirements = typename Physics<%(Dimension)s>::ConnectivityRequirements;
   using PairAccelerationsType = PairwiseField<%(Dimension)s, Vector>;
 """
     
