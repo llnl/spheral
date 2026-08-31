4993d5614ce4b0ecafaff5568276c2eb0436727c
Mike Owen <mikeowen@llnl.gov>
2026-08-31 15:44:53 -0700

    Adding ostream operator for RKOrder

--- src/RK/RKCorrectionParams.hh
+++ src/RK/RKCorrectionParams.hh
@@ -21,5 +21,24 @@ enum class RKOrder : int {//Used to assign the order of the corrections
 // Backward compatibility
 using RKVolumeType = VolumeType;
 
+//------------------------------------------------------------------------------
+// ostream operator for RKOrder
+//------------------------------------------------------------------------------
+inline
+std::ostream& operator<<(std::ostream& os, const RKOrder& x) {
+  switch(x) {
+  case RKOrder::ZerothOrder:    os << "ZerothOrder"; break;
+  case RKOrder::LinearOrder:    os << "LinearOrder"; break;
+  case RKOrder::QuadraticOrder: os << "QuadraticOrder"; break;
+  case RKOrder::CubicOrder:     os << "CubicOrder"; break;
+  case RKOrder::QuarticOrder:   os << "QuarticOrder"; break;
+  case RKOrder::QuinticOrder:   os << "QuinticOrder"; break;
+  case RKOrder::SexticOrder:    os << "SexticOrder"; break;
+  case RKOrder::SepticOrder:    os << "SepticOrder"; break;
+  default:                      os << "Unknown RKOrder";
+  }
+  return os;
+}
+
 }
 #endif
