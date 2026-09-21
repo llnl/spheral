ae231c3c87f412610aa136ac616e0ab13f5629be
Mike Owen <mikeowen@llnl.gov>
2026-09-21 13:39:38 -0700

    Cleaning up

--- src/PYB11/ArtificialViscosity/TensorMonaghanGingoldViscosity.py
+++ src/PYB11/ArtificialViscosity/TensorMonaghanGingoldViscosity.py
@@ -44,31 +44,3 @@ using VolumeRequirements = typename Physics<%(Dimension)s>::VolumeRequirements;
     @PYB11const
     def label(self):
         return "std::string"
-
-# from ArtificialViscosityView import *
-# from ArtificialViscosityAbstractMethods import *
-# @PYB11template("Dimension")
-# @PYB11template_dict({"QPiType": "typename %(Dimension)s::Tensor"})
-# class TensorMonaghanGingoldViscosityView(ArtificialViscosityView):
-
-#     PYB11typedefs = """
-#     using Scalar = typename %(Dimension)s::Scalar;
-#     using Vector = typename %(Dimension)s::Vector;
-#     using Tensor = typename %(Dimension)s::Tensor;
-#     using SymTensor = typename %(Dimension)s::SymTensor;
-#     using ThirdRankTensor = typename %(Dimension)s::ThirdRankTensor;
-#     using TimeStepType = typename Physics<%(Dimension)s>::TimeStepType;
-#     using ResidualType = typename Physics<%(Dimension)s>::ResidualType;
-#     using ReturnType = %(QPiType)s;
-# """
-
-#     #...........................................................................
-#     # Constructors
-#     def pyinit(self,
-#                Clinear = "const Scalar",
-#                Cquadratic = "const Scalar"):
-#         "TensorMonaghanGingoldViscosityView constructor"
-# #-------------------------------------------------------------------------------
-# # Inject abstract interface
-# #-------------------------------------------------------------------------------
-# PYB11inject(ArtificialViscosityAbstractMethods, TensorMonaghanGingoldViscosityView, virtual=True, pure_virtual=False)
